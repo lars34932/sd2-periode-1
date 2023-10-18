b96c03cfd48c704acb7333ee3cdea247eefd5aee
lars34932 <34932@ma-web.nl>
2023-10-18 11:23:09 +0200

    fotos

--- source/views/generateCta.php
+++ source/views/generateCta.php
@@ -4,7 +4,7 @@ function makeCta() {
     <section class="cta__section">
         <article class="cta__article">
             <p class="cta__text">Speel hier de SDG game en kom er achter wat elke SDG inhoud</p>
-            <button class="cta__button">Speel</button>
+            <a href="https://sdggame.nl/"><button class="cta__button">Speel</button></a>
         </article>
     </section>';
 }
