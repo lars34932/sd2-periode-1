1d00c4172a30dabdec50309c1ee71bf6963c6cdb
MikaDeWilde <mikadewilde.01@gmail.com>
2023-10-18 12:25:13 +0200

    footer idee af

--- public/index.php
+++ public/index.php
@@ -33,7 +33,7 @@
         <a class="footer--hover" href="">+06123456789</a>
         <a class="footer--hover" href="">contactweg 36</a>
         <a class="footer--hover" href="">mimola@gmail.com</a>
-        <a class="footer--hover" href="">instagram</a>
+        <a class="footer--hover footer--gap" href="">instagram</a>
     </footer>
 </body>
 </html>
