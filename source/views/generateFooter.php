acaf83ef732b594857c7923d600b0ec613e6297e
lars34932 <34932@ma-web.nl>
2023-10-31 13:55:17 +0100

    klaar

--- source/views/generateFooter.php
+++ source/views/generateFooter.php
@@ -1,7 +1,7 @@
 <?php
 function makeFooter() {
 echo '<footer>
-    <a class="footer--hover" href="">06123456789</a>
+    <a class="footer--hover" href="">06-123456789</a>
     <a class="footer--hover" href="">contactweg 36</a>
     <a class="footer--hover" href="">mimola@gmail.com</a>
     <a class="footer--hover footer--gap" href="">instagram</a>
